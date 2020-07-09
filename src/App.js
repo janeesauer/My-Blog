56dc59dc3b20b9fc126999fb4e2f14413c0b76e4
janeesauer <55198243+janeesauer@users.noreply.github.com>
2020-07-09 10:15:07 +0100

    added article content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+import NavBar from './NavBar';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<NavBar />
+				<div id='page-body'>
+					<Route path='/' component={HomePage} exact />
+					<Route path='/about' component={AboutPage} />
+					<Route path='/articles-list' component={ArticlesList} />
+					<Route path='/article/:name' component={ArticlePage} />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<>
+		<h1>About me</h1>
+		<p>
+			{' '}
+			bla bla bla paragraph 1 bla bla bla paragraph 1 bla bla bla
+			paragraph 1 bla bla bla paragraph 1 bla bla bla paragraph 1 bla bla
+			bla paragraph 1 bla bla bla paragraph 1 bla bla bla paragraph 1
+		</p>
+		<p> bla bla bla paragraph 2</p>
+		<p> bla bla bla paragraph 3</p>
+	</>
+);
+
+export default AboutPage;
